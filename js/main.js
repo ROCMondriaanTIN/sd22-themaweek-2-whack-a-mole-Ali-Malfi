da7f39237248e7f162831fc5289b9570d2198d61
Ali-Malfi <115076598+Ali-Malfi@users.noreply.github.com>
2023-01-17 10:20:57 +0100

    tileclicked functie en evenlistener to gevoegd

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ let playerPoints =0;
 
 const playerPointsElement = document.querySelector(".player-points");
 const allTiles = document.querySelectorAll(".tile");
+
+getRandomNumber(1, 50);
+function getRandomNumber(min, max){
+    // let random= Math.random();
+    // console.log(random);
+    // console.log(min);
+    // console.log(max);
+    // console.log(Math.floor(random));
+    // console.log(max - min +1);
+    // console.log(Math.floor(random * (max - min + 1)));
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+allTiles.forEach(function(tile){
+    console.log(tile);
+    tile.addEventListener("click", function(){
+        tileclick(tile);
+    });
+});
+
+function tileclick(tile) {
+    console.log(tile);
+}
