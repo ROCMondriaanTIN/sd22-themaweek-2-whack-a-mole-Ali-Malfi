6d50b2da39a992e7f19710b717ac0d164486c7b2
Ali-Malfi <115076598+Ali-Malfi@users.noreply.github.com>
2023-01-17 09:32:32 +0100

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log("loaded");
+const minimumTime = 500;
+const maximumTime = 1250;
+
+let playerPoints =0;
+
+const playerPointsElement = document.querySelector(".player-points");
+const allTiles = document.querySelectorAll(".tile");
